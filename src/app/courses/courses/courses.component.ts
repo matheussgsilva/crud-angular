5d640e1eb1d997529980c61467c7baa630376ac6
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-15 20:34:24 -0300

    Create dialog to show the error message

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,8 @@
-import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
@@ -12,9 +15,24 @@ export class CoursesComponent implements OnInit {
 
   public courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
+ 
+  constructor(
+      private coursesService: CoursesService,
+      public dialog: MatDialog
+    ) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar os cursos.');
+        return of([])
+      })
+    );
+  }
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
